9d5010a216b2de89d445f0c8752263334df2ef5d
BiagioButta <biagiob940@gmail.com>
2022-10-15 23:00:31 +0200

    manca html secondo es

--- js/script.js
+++ js/script.js
@@ -1,57 +1,80 @@
 "use strict";
 
-// Funzione che genera stringhe casuali con lunghezza stabilita
+const slideHTML =document.querySelectorAll('.slide');
+const prevBtnHTML =document.querySelector('.prevBtn');
+const nextBtnHTML =document.querySelector('.nextBtn');
 
-const minuscole=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","u","v","w","x","y","z"];
 
-let parolaGenerataHTML = document.getElementById('parolaGenerata');
-let genera = document.getElementById('generaParola');
+// forEach mi assegna la funzione ad ogni elemento
+// la funzione mi indica di quanto sarà spostata la slide a sinistra
+// a 100% si puù sostituire un altra grandezza
 
-function generaParolaCasuale(lunghezzaParola) {
-    let parolaGenerataHTML = '';
-    for (let i = 0; i < lunghezzaParola; i++) {
-      let parola = Math.floor(Math.random() * minuscole.length);
-      parolaGenerataHTML += minuscole[parola];
-    }
-    return parolaGenerataHTML;
+slideHTML.forEach( function(slide, spazioASinistra){
+  slide.style.left = `${spazioASinistra * 100}%`
+});
+
+
+let numeroPagina = 0;
+
+// al click viene aumentato o diminuito il numeroPagina e viene applicata la funzione
+nextBtnHTML.addEventListener('click', function(){
+  numeroPagina++;
+  scorrerePagine();
+});
+prevBtnHTML.addEventListener('click', function(){
+  numeroPagina--;
+  scorrerePagine();
+});
+
+
+function scorrerePagine(){
+
+  // viene aggiunto - 1 altrimenti alla fine resterebbe una pagina vuota
+  if(numeroPagina < slideHTML.length - 1){
+    nextBtnHTML.style.display = 'block';
+  }else{
+    nextBtnHTML.style.display = 'none';
+  }
+
+  if(numeroPagina > 0){
+    prevBtnHTML.style.display = 'block';
+  }else{
+    prevBtnHTML.style.display = 'none';
+  }
+
+  // quando viene applicata la funzione la pagina si sposta dei parametri assegnati
+  slideHTML.forEach( function(slide){
+    slide.style.transform = `translateX(-${numeroPagina * 100}%)`
+  })
 }
 
-let parolaRandom = generaParolaCasuale(0);
-parolaGenerataHTML.innerHTML = parolaRandom;
-console.log(parolaRandom);
+prevBtnHTML.style.display = 'none';
 
 ///////////////////////////////////////////
 
 //Inserisco una parola
-let inputParola = prompt(' Inserisci una parola');
+let inputParolaHTML =document.getElementById('inputParola.value');
+let verificaPalinromaHTML =document.getElementById('verificaPalindroma');
+let esitoHTML = document.getElementById('esito');
 
-// if (!isNaN(inputParola)) {
-//   // stampo messaggio di errore
-//   console.log('Attenzione! Non hai inserito una parola!');
-//   // altrimenti
-// } else {
-//   // stampo la parola inserita
-//   console.log(inputParola);
-// }
 
-// funzione che la inverte
-function invertiParola(stringa){
-  let invertiStringa = stringa.split('').reverse().join('');  
-  // split trasforma la parola in array
-  // reverse lo inverte
-  // join riporta la stringa
-  return invertiStringa;
+function invertiStringa(stringa) {
+  let parola = stringa.split('').reverse().join('');
+  return parola;
 }
 
-let parolaInversa = invertiParola(inputParola);
 
-if(inputParola == parolaInversa){
-    console.log('la parola è palindroma');
-  } else {
-    console.log('la parola non è palindroma');
+verificaPalinromaHTML.addEventListener ('click', function(){
+  invertiStringa();
+  if(invertiStringa(inputParolaHTML)){
+    esitoHTML.innerHTML = 'La parola è palindroma'
+  }else{
+    esitoHTML.innerHTML = 'La parola non è palindroma'
   }
+  esitoHTML.innerHTML= '';
+})
   
-////////////////////////////////////////////
+// ////////////////////////////////////////////
 
 
 // creo un flag vero
@@ -112,11 +135,11 @@ if (somma%2 == 0) {
 
 // dichiaro il vincitore
 if (scelta == "pari" && somma%2 == 0) {
-  console.log("Il vincitore sei tu! :) ");
+  console.log("Il vincitore sei tu ");
 } else if (scelta == "pari" && somma%2 == 1){
-  console.log("Vince il computer :( ");
+  console.log("Vince il computer ");
 } else if(scelta == "dispari" && somma%2 == 0){
-  console.log("Vince il computer :( ");
+  console.log("Vince il computer");
 } else {
-  console.log("Il vincitore sei tu! :) ");
+  console.log("Il vincitore sei tu!dispari ");
 }
